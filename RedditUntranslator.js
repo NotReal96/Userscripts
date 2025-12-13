e8adee2a4db27c800367d883da5a870a3f3f9103
NotReal96 <58608665+NotReal96@users.noreply.github.com>
2025-12-13 22:19:04 +0100

    Update RedditUntranslator.js
    Added the removal of the "tr" parameter from the query string

--- RedditUntranslator.js
+++ RedditUntranslator.js
@@ -1,18 +1,30 @@
 // ==UserScript==
 // @name			NR Reddit Untranslator
-// @version			1.0.0.0
-// @description		Toggles Reddit's automatic translations off when loading the page (useful in incognito mode)
+// @version			1.1.0.0
+// @description		Toggles Reddit's automatic translations off when loading the page and removes "tl" from the query string (useful in incognito mode)
 // @author			NotReal96
 // @license			https://github.com/NotReal96/Userscripts/blob/main/LICENSE-C
 // @match			https://www.reddit.com/*
 // @icon			https://www.redditstatic.com/shreddit/assets/favicon/64x64.png
+// @run-at			document-start
 // ==/UserScript==
 
-let x,y=(r,s)=>{
+let D=document,y,x=new URL(location.href),T=200,t,z,w
+if(x.searchParams.has("tl"))location.replace(x.searchParams.delete("tl"),x)
+y=(r,s)=>{
 	let R=r.querySelector(s)
 	if(!R)for(let h of r.querySelectorAll("*"))if((h=h.shadowRoot)&&h.mode=="open")if(R=y(h,s))break
 	return R
-},T=200,t=setInterval(f=>{
-	if((x=y(document,"#hui-translation-switch"))||!--T)clearInterval(t)
-	if(x?.checked)x.shadowRoot.querySelector(".switch-input-primary").click()
-},1)
+}
+z=f=>{
+	if((x=y(D,"#hui-translation-switch"))&&x.checked)x.shadowRoot.querySelector(".switch-input-primary").click()
+	return x
+}
+D.addEventListener("DOMContentLoaded",f=>{
+	t=setInterval(f=>{
+		if(z()||!--T){
+			clearInterval(t)
+			if(!T)D.addEventListener("load",z)
+		}
+	},1)
+})
